a9573edd2b079d4033527c81d01535b29fc3dcd7
David James <123336283+davidjamesdimalanta@users.noreply.github.com>
2023-01-23 20:16:23 -0500

    Update PortfolioItem.jsx
    link now works on image click

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -3,11 +3,12 @@ import React from "react";
 function PortfolioItem({title, imgUrl, stack, link}) {
     return (
         <div className="dark:border-white border-2 border-stone-900 rounded-md overflow-hidden">
+            <a href={link}>
             <img 
             src={imgUrl} 
             alt="portfolio"
             className="w-full h-36 md:h-48 object-cover cursor-pointer"
-             />
+             /> </a>
             <div className="w-full p-4">
                 <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-white">{title}</h3>
                 <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
