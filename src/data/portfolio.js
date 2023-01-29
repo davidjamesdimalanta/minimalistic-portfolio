dd79b7e0c3116b9e9cd59609d8b9559cafa32d5e
David James <123336283+davidjamesdimalanta@users.noreply.github.com>
2023-01-28 19:46:07 -0500

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,7 +2,13 @@ export default [
     {
         title: 'UofT Booking Project',
         imgUrl: '/assets/bookingspreview.png',
-        stack: ['CSS','ReactJS'],
+        stack: ['CSS','React'],
         link: 'https://davidjamesuoftbookings.netlify.app/',
+    },
+    {
+        title: 'Product Website',
+        imgUrl: '/assets/bookingspreview.png',
+        stack: ['TailwindCSS','React'],
+        link: 'https://davidjamesdimalanta-product-website.netlify.app',
     },
 ]
